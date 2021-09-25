ffffe131f9630ae4d25120dbc1093a183a85d3f2
Cheng JIANG <alex_cj96@foxmail.com>
2021-09-25 13:48:00 +0800

    fix loans benchmarks build for mock runtime

--- pallets/loans/src/mock.rs
+++ pallets/loans/src/mock.rs
@@ -37,7 +37,7 @@ construct_runtime!(
         Balances: pallet_balances::{Pallet, Call, Storage, Event<T>},
         Loans: crate::{Pallet, Storage, Call, Config, Event<T>},
         TimestampPallet: pallet_timestamp::{Pallet, Call, Storage, Inherent},
-        Assets: pallet_assets::<Instance1>::{Pallet, Call, Storage, Event<T>},
+        Assets: pallet_assets::{Pallet, Call, Storage, Event<T>},
     }
 );
 
@@ -155,8 +155,7 @@ parameter_types! {
     pub const MetadataDepositPerByte: u64 = 1;
 }
 
-type AssetsInstance = pallet_assets::Instance1;
-impl pallet_assets::Config<AssetsInstance> for Test {
+impl pallet_assets::Config for Test {
     type Event = Event;
     type Balance = Balance;
     type AssetId = AssetIdentifier;
