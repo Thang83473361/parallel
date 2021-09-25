84145baa9860691694de47b9e4bd9bbb63176bcd
GopherJ <GopherJ@users.noreply.github.com>
2021-09-25 03:58:30 +0000

    deploy: f8cf8061fbfdc4078495922728b513a016386a86

---
+++ implementors/parallel_primitives/trait.AMM.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_amm"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_amm/pallet/trait.Config.html\" title=\"trait pallet_amm::pallet::Config\">Config</a>&lt;I&gt;, I:&nbsp;'static&gt; <a class=\"trait\" href=\"parallel_primitives/trait.AMM.html\" title=\"trait parallel_primitives::AMM\">AMM</a>&lt;T&gt; for <a class=\"struct\" href=\"pallet_amm/pallet/struct.Pallet.html\" title=\"struct pallet_amm::pallet::Pallet\">Pallet</a>&lt;T, I&gt;","synthetic":false,"types":["pallet_amm::pallet::Pallet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ parallel_primitives/tokens/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["DOT",""],["HKO",""],["KSM",""],["PARA",""],["USDT",""],["XDOT",""],["XKSM",""]]});
